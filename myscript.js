0c6eff823327cea6a8aa449c6671ddb8d40e0e92
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-12 16:27:50 +0200

    mofied const and let factors

--- myscript.js
+++ myscript.js
@@ -1,28 +1,27 @@
 // ? Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
 // ? Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
+//? il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//? va applicato uno sconto del 20% per i minorenni 
+//? va applicato uno sconto del 40% per gli over 65.
 
-L'output del prezzo finale va messo fuori in forma umana in pagina (con massimo due decimali, per indicare centesimi sul prezzo).
-Questo richiederà un minimo di ricerca.
+//? L'output del prezzo finale va messo fuori in forma umana in pagina (con massimo due decimali, per indicare centesimi sul prezzo).
 
 
 let  userkilometers = parseInt(prompt ('Insert kilometers'));
 
 
-let userAge = parseInt(prompt ('Insert kilometers'));
+let userAge = parseInt(prompt ('Insert your agexd'));
 
 const adult = 18
 
 const major = 65
 
-if()
+// ? if()
 
 
 
 
-let UserInfo =;
+// ? let UserInfo =;
 
 document.getElementById('ticket-price') .innerHTML = UserInfo;
